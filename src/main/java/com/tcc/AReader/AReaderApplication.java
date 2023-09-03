cae9b17df7790f0211c11beb8d25747f8a5b5a92
Rsironi <ruansironi@hotmail.com>
2023-09-03 17:55:25 -0300

    Initial Commit

---
+++ src/main/java/com/tcc/AReader/AReaderApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.AReader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc/AReader/AReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.AReader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
