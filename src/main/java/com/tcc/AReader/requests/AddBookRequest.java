7fcc41b6339922e88c37295afb05c80d005d3677
Rsironi <ruansironi@hotmail.com>
2023-10-14 04:39:51 -0300

    AddBookRequest removed useless lombok Annotations

--- src/main/java/com/tcc/AReader/requests/AddBookRequest.java
+++ src/main/java/com/tcc/AReader/requests/AddBookRequest.java
@@ -4,13 +4,9 @@ import org.hibernate.validator.constraints.ISBN;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor(staticName = "build")
-@NoArgsConstructor
 public class AddBookRequest {
   @Email(message = "Email inválido")
   @NotEmpty(message = "Email não pode ser vazio")
